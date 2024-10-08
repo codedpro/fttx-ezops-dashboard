25d790bb427749f429441aec3ee2be5d81c8f2c9
CodedPro <dev.codedpro@gmail.com>
2024-10-08 18:56:41 +0330

    uncomment design desk

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -11,7 +11,13 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import SidebarItem from "@/components/Sidebar/SidebarItem";
-import { PiDesk, PiMapPinArea, PiPaintBrush, PiPaintBrushBroad, PiPaintBrushBroadDuotone } from "react-icons/pi";
+import {
+  PiDesk,
+  PiMapPinArea,
+  PiPaintBrush,
+  PiPaintBrushBroad,
+  PiPaintBrushBroadDuotone,
+} from "react-icons/pi";
 interface SidebarProps {
   sidebarOpen: boolean;
   setSidebarOpen: (arg: boolean) => void;
@@ -80,11 +86,11 @@ const SidebarComponent = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       icon: <PiMapPinArea />,
       route: "/iranfttx",
     },
-/*      {
-        label: "Design Desk",
-        icon: <FaPaintBrush />,
-        route: "/design-desk",
-      },  */
+    {
+      label: "Design Desk",
+      icon: <FaPaintBrush />,
+      route: "/design-desk",
+    },
   ];
 
   if (!sidebarOpen) {
